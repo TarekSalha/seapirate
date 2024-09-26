baef32bb18a464fa04c9fa4584f8a4649ebec1eb
Tarek Salha <tarek.salha.ext@glasfaserplus.de>
2024-09-26 11:06:07 +0200

    changed some infra stuff;

--- seapirate.infrastructure/seapirate.cs
+++ seapirate.infrastructure/seapirate.cs
@@ -7,6 +7,7 @@ using Pulumi.AzureNative.Sql;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Insights;
 using Pulumi.AzureNative.Sql.Inputs;
+using Pulumi.AzureNative.OperationalInsights;
 
 class SeaPirateApp : Stack
 {
@@ -49,7 +50,7 @@ class SeaPirateApp : Stack
 
         // Connection string for the SQL Database
         var connectionString = Output.Tuple(sqlServer.Name, sqlDatabase.Name).Apply(names =>
-            $"Server=tcp:{names.Item1}.database.windows.net,1433;Initial Catalog={names.Item2};Persist Security Info=False;User ID=adminuser;Password=Password123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            $"Server=tcp:{names.Item1}.database.windows.net,1433;Initial Catalog={names.Item2};Persist Security Info=False;User ID=chefpirate;Password=P!ratesAre2Hungry;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
         // Create an Azure App Service Plan for the ASP.NET Core backend
         var appServicePlan = new AppServicePlan("seapirate-appservice-plan", new AppServicePlanArgs
@@ -106,12 +107,26 @@ class SeaPirateApp : Stack
             }
         });
 
+        var logAnalyticsWorkspace = new Workspace("seapirate-loganalyticsworkspace", new WorkspaceArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Location = resourceGroup.Location,
+            Sku = new Pulumi.AzureNative.OperationalInsights.Inputs.WorkspaceSkuArgs
+            {
+                Name = WorkspaceSkuNameEnum.PerGB2018
+            },
+            RetentionInDays = 30,
+        });
+
         // Create Application Insights for backend monitoring
         var appInsights = new Component("seapirate-appinsights", new ComponentArgs
         {
             ResourceGroupName = resourceGroup.Name,
             ApplicationType = "web",
-            Location = resourceGroup.Location
+            Kind = "web",
+            Location = resourceGroup.Location,
+            IngestionMode = "LogAnalytics",
+            WorkspaceResourceId = logAnalyticsWorkspace.Id
         });
 
         // Output the backend URL, frontend URL, and SQL connection string
